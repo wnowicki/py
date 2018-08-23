0f1291b07b37e7b2ccbee10d02cf5bf41918262a
Wojtek <wojciech.nowicki@paybreak.com>
2018-08-23 11:22:21 +0100

    Documentation standards

--- configuration.py
+++ configuration.py
@@ -1,3 +1,16 @@
+#!/usr/bin/env python
+
+"""Simple configuration helper"""
+
+__author__ = "Wojciech Nowicki"
+__copyright__ = "Copyright 2018, wojciechnowicki.com"
+__credits__ = []
+__license__ = "GPL"
+__version__ = "0.1.0"
+__maintainer__ = "Wojciech Nowicki"
+__email__ = "contact@wojciechnowicki.com"
+__status__ = "Development"
+
 import json
 
 
